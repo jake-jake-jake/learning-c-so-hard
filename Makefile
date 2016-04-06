743a6017a518380411b737828f1a8d670ff3ab1a
Jake <jcdavis@awedge.net>
2016-04-06 11:27:28 -0400

    Add exercises 4 and 5

--- Makefile
+++ Makefile
@@ -3,5 +3,6 @@ CFLAGS=-Wall -g
 clean:
 	rm -f ex01
 	rm -f ex03
+	rm -f ex04
 
-all: ex01 ex03
+all: ex01 ex03 ex04
---
+++ ex04.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* Warning: This program is wrong on purpose. */
+
+int main()
+{
+    int age = 32;
+    int height = 68;
+    printf("I am %d years old.\n", age);
+    printf("I am %d inches tall.\n", height);
+
+    return 0;
+}
