2c388a701609be6672afceb519468d0a0902c618
Jake <jcdavis@awedge.net>
2016-04-25 16:41:02 -0400

    Exercise 16 extra credit

--- ex16-extra.c
+++ ex16-extra.c
@@ -23,6 +23,16 @@ struct Person *Person_create(char *name, int age, int height, int weight)
     return who;
 }
 
+struct Person Person_create_stack(char *name, int age, int height, int weight)
+{
+    struct Person who;
+    who.name = strdup(name);
+    who.age = age;
+    who.height = height;
+    who.weight = weight;
+    return who;
+}
+
 void Person_destroy(struct Person *who)
 {
     assert(who != NULL);
@@ -30,6 +40,16 @@ void Person_destroy(struct Person *who)
     free(who->name);
     free(who);
 }
+
+// void Person_destroy_stack(struct Person who)
+// {
+//     assert(who.name != NULL);
+
+//     free(who);
+//     // free(who.age);
+//     // free(who.height);
+//     // free(who.weight);
+// }
 
 void Person_print(struct Person *who)
 {
@@ -39,25 +59,39 @@ void Person_print(struct Person *who)
     printf("\tWeight: %d\n", who->weight);
 }
 
+void Person_print_stack(struct Person who)
+{
+    printf("Name: %s\n", who.name);
+    printf("\tAge: %d\n", who.age);
+    printf("\tHeight: %d\n", who.height);
+    printf("\tWeight: %d\n", who.weight);
+}
+
 int main(int argc, char *argv[])
 {
     // make two people structures
     struct Person *joe = Person_create(
             "Joe Alex", 32, 64, 140);
-
-    struct Person *frank = Person_create(
-            "Frank Blank", 20, 72, 180);
-
-    struct Person *null = NULL;
-
-    // print them out and where they are in memory
     printf("Joe is at memory location %p:\n", joe);
     Person_print(joe);
     printf("---\n");
 
+    struct Person *frank = Person_create(
+            "Frank Blank", 20, 72, 180);
     printf("Frank is at memory location %p:\n", frank);
     Person_print(frank);
     printf("---\n");
+
+
+    struct Person stacked_person = Person_create_stack(
+            "On the stack", 24, 70, 165);
+    printf("stacked_person is at memory location: %p\n", &stacked_person);
+    Person_print_stack(stacked_person);
+
+    int some_int = 100;
+    printf("some_int is at memory location: %p\n", &some_int);
+
+
 
     // make everyone age 2- years and print them again
     joe->age += 20;
@@ -74,7 +108,6 @@ int main(int argc, char *argv[])
     // destroy them both so we clean up
     Person_destroy(joe);
     Person_destroy(frank);
-    Person_destroy(null);
 
     return 0;
 }
